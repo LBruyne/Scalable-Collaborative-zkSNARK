f0f99d96046e5084c137f6cc65a91b492e16a18e
AsterNighT <asternight@outlook.com>
2024-05-15 09:30:13 +0000

    Update rng
    Signed-off-by: AsterNighT <asternight@outlook.com>

--- hyperplonk/src/lib.rs
+++ hyperplonk/src/lib.rs
@@ -1,10 +1,11 @@
 use ark_ff::UniformRand;
-
+use rand::{rngs::StdRng, SeedableRng};
 pub mod hyperplonk;
 pub mod dhyperplonk;
 
 pub fn random_evaluations<F: UniformRand>(n: usize) -> Vec<F> {
+    let rng = &mut StdRng::from_entropy();
     (0..n)
-        .map(|_| F::rand(&mut ark_std::test_rng()))
+        .map(|_| F::rand(rng))
         .collect::<Vec<_>>()
 }
